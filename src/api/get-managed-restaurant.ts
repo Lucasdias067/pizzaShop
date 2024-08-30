f5d17d0d9ce8610a433261f0eb195c737c981642
Lucas Dias <lucasdias067@gmail.com>
2024-08-29 21:09:35 -0300

    first commit

---
+++ src/api/get-managed-restaurant.ts
@@ -0,0 +1,17 @@
+import { api } from '@/lib/axios'
+
+interface getManagedRestaurantResponse {
+  name: string
+  id: string
+  description: string | null
+  createdAt: Date | null
+  updatedAt: Date | null
+  managerId: string | null
+}
+
+export async function getManagedRestaurant() {
+  const response = await api.get<getManagedRestaurantResponse>(
+    '/managed-restaurant',
+  )
+  return response.data
+}
